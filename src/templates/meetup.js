1cc42be39dedde36d97e3ac0bba384976eea8b3d
Robert Cooper <robert.cooper.qu@gmail.com>
2018-12-02 15:31:52 -0500

    Fix broken build

--- src/templates/meetup.js
+++ src/templates/meetup.js
@@ -22,25 +22,25 @@ class MeetupTemplate extends Component {
         </div>
         <div className="meetup-presenters">
           {this.props.meetup.presenters.map(presenter => (
-            <div className="meetup-presenter" key={presenter.name}>
-              <div className="meetup-presenterImageContainer">
+            <div className="meetup-presenter meetupUnit" key={presenter.name}>
+              <div className="meetup-presenterImageContainer  meetupUnit-imageContainer">
                 <img
-                  className="meetup-presenterImage"
+                  className="meetup-presenterImage  meetupUnit-image"
                   src={presenter.image ? presenter.image : HeadshotPlaceholder}
                   alt={presenter.image ? presenter.name : "Default headshot placeholder"}
                 />
-                <span className="meetup-presenterName">{presenter.name}</span>
+                <span className="meetup-presenterName  meetupUnit-name">{presenter.name}</span>
               </div>
-              <div className="meetup-presenterInfo">
+              <div className="meetup-presenterInfo  meetupUnit-info">
                 {presenter.presentationTitle && (
                   <h4 className="meetup-presenterTitle">{presenter.presentationTitle}</h4>
                 )}
-                <p className="meetup-presenterText">{presenter.text}</p>
-                <ul className="meetup-presenterLinks">
+                <p className="meetup-presenterText  meetupUnit-text">{presenter.text}</p>
+                <ul className="meetup-presenterLinks  meetupUnit-links">
                   {presenter.links &&
                     presenter.links.map((link, index) => (
-                      <li key={index} className="meetup-presenterLinkItem">
-                        <a className="meetup-presenterLink" href={link.linkURL}>
+                      <li key={index} className="meetup-presenterLinkItem  meetupUnit-linkItem">
+                        <a className="meetup-presenterLink  meetupUnit-link" href={link.linkURL}>
                           {link.linkText}
                         </a>
                       </li>
@@ -50,21 +50,23 @@ class MeetupTemplate extends Component {
             </div>
           ))}
         </div>
+        <h3 className="meetup-sponsorsTitle">Thanks to our Sponsors</h3>
         <div className="meetup-sponsors">
-            <h3>Thanks to our Sponsors</h3>
-            <div className="meetup-sponsor">
-              <img className="meetup-sponsorImage" src={HeadshotPlaceholder} alt={"Default headshot placeholder"} />
-              <h4 className="meetup-sponsorName">Shopify</h4>
-            </div>
-            <div className="meetup-sponsorInfo">
-              <p className="meetup-sponsorText">Thanks to Shopify for sponsoring the venue location.</p>
-              <ul className="meetup-sponsorLinks">
-                <li className="meetup-sponsorLinkItem">
-                  <a className="meetup-sponsorLink" href="#">
-                    Work at Shopify
-                  </a>
-                </li>
-              </ul>
+            <div className="meetup-sponsor  meetupUnit">
+              <div className="meetupUnit-imageContainer">
+                <img className="meetupUnit-image" src={HeadshotPlaceholder} alt={"Default headshot placeholder"} />
+                <h4 className="meetupUnit-name">Shopify</h4>
+              </div>
+              <div className="meetupUnit-info">
+                <p className="meetupUnit-text">Thanks to Shopify for sponsoring the venue location.</p>
+                <ul className="meetupUnit-links">
+                  <li className="meetupUnit-linkItem">
+                    <a className="meetupUnit-link" href="#">
+                      Work at Shopify
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
         </div>
       </section>
