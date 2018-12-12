250c5aaac8e31cee2bee1980e7dd201fc9f25486
Robert Cooper <robert.cooper.qu@gmail.com>
2018-12-11 21:51:46 -0500

    Add mobile hamburger menu 🍔

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,27 +3,53 @@ import React from "react";
 import "./styles.scss";
 import CustomLink from "../CustomLink";
 
-export const NavbarTemplate = ({ data, currentPage }) => (
-  <nav className="navbar">
-    <div className="container  navbar-container">
-      {data.menuItems.length > 0 && (
-        <ul className="navbar-menu">
-          {data.menuItems.map((menuItem, index) => (
-            <li key={menuItem.linkURL + index} className="navbar-menuItem">
-              <CustomLink
-                linkType={menuItem.linkType}
-                linkURL={menuItem.linkURL}
-                className={currentPage === menuItem.linkURL ? "navbar-menuLink  navbar-menuLink--active" : "navbar-menuLink"}
-              >
-                {menuItem.label}
-              </CustomLink>
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-  </nav>
-);
+const Hamburger = ({onClick, isOpen}) => (
+  <div className={`navbar-hamburger${isOpen ? ' navbar-hamburger--open' : ''}`} onClick={onClick}>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
+)
+
+
+export class NavbarTemplate extends React.Component {
+  state = {
+    isOpen: false
+  }
+
+  handleMenuToggle = () => {
+    this.setState((state) => ({isOpen: !state.isOpen}))
+  }
+
+  render() {
+    const { data, currentPage } = this.props;
+    return (
+      <nav className={`navbar${this.state.isOpen ? ' navbar--open' : ''}`}>
+        <div className={`navbar-container${this.state.isOpen ? ' navbar-container--open'  : ''}`}>
+          {data.menuItems.length > 0 && (
+            <ul className={`navbar-menu${this.state.isOpen ? ' navbar-menu--open'  : ''}`}>
+              {data.menuItems.map((menuItem, index) => (
+                <li key={menuItem.linkURL + index} className="navbar-menuItem">
+                  <CustomLink
+                    linkType={menuItem.linkType}
+                    linkURL={menuItem.linkURL}
+                    className={currentPage === menuItem.linkURL ? "navbar-menuLink  navbar-menuLink--active" : "navbar-menuLink"}
+                  >
+                    {menuItem.label}
+                  </CustomLink>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+        <Hamburger onClick={this.handleMenuToggle} isOpen={this.state.isOpen}/>
+      </nav>
+    );
+  }
+}
 
 const Navbar = props => {
   if (!props.data) {
